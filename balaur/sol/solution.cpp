da2e4977ec6ff1a823df9fb6f7d8c5080744ab64
ItsHezov <140702963+ItsHezov@users.noreply.github.com>
2025-02-09 00:25:25 +0200

    Add files via upload

---
+++ balaur/sol/solution.cpp
@@ -0,0 +1,11 @@
+#include <fstream>
+using namespace std;
+ifstream cin("balaur.in");
+ofstream cout("balaur.out");
+int main()
+{
+    long long n;
+    cin >> n;
+    cout << 5 * n;
+	return 0;
+}
